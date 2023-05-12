f394b333533fdb98e39505b08ac147c66f55af45
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-05-12 16:15:12 +0530

    fixed expense activity

---
+++ app/src/main/java/com/example/vyay_expense_tracker/ExpenseActivity.java
@@ -0,0 +1,14 @@
+package com.example.vyay_expense_tracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
